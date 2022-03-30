72f5fee52df795cb711cbdea778467da7b454ea4
J. Christian Romero <jcromerohdz@gmail.com>
2022-03-30 09:26:40 -0700

    Setup MUI framework

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-
+import { Container, Grid } from '@mui/material'
+import Typography from '@mui/material/Typography';
 function App() {
 
   return (
-    <h1>App News</h1>
+		<Container>
+			<header>
+				<Typography variant="h3" component="h1" align="center" marginY={5}>
+        	Find News
+      	</Typography>
+			</header>
+		</Container>
   )
 }
 
